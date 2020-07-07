893400beb4dff269d87e2d727cba9479b16e907f
Victor <kuro_caelum@outlook.com>
2020-07-07 10:27:21 -0300

    Frontend finalizado

---
+++ frontend/src/about/about.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+import PageHeader from "../template/pageHeader";
+
+export default (props) => (
+  <div>
+    <PageHeader name="Sobre" small="Nós"></PageHeader>
+
+    <h2>Nossa história</h2>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis cumque
+      facilis ducimus aperiam incidunt maxime similique? Exercitationem odio cum
+      quia perspiciatis sint explicabo quibusdam maxime. Quisquam aut rerum
+      ratione quo.
+    </p>
+    
+    <h2>Missão e Visão</h2>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis cumque
+      facilis ducimus aperiam incidunt maxime similique? Exercitationem odio cum
+      quia perspiciatis sint explicabo quibusdam maxime. Quisquam aut rerum
+      ratione quo.
+    </p>
+    
+    <h2>Imprensa</h2>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis cumque
+      facilis ducimus aperiam incidunt maxime similique? Exercitationem odio cum
+      quia perspiciatis sint explicabo quibusdam maxime. Quisquam aut rerum
+      ratione quo.
+    </p>
+
+  </div>
+);
