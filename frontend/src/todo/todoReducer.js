b40f08f64461e770ba0104a9db615c3045b57cad
Victor <kuro_caelum@outlook.com>
2020-07-16 16:39:03 -0300

    Reducer modularizado

---
+++ frontend/src/todo/todoReducer.js
@@ -0,0 +1,31 @@
+const INITIAL_STATE = {
+  description: "Ler livro",
+  list: [
+    {
+      _id: 1,
+      description: "Pagar cartão",
+      done: true,
+    },
+    {
+      _id: 2,
+      description: "Reunião com equipe",
+      done: false,
+    },
+    {
+      _id: 3,
+      description: "Consulta médica",
+      done: false,
+    },
+  ],
+};
+
+export default (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    case "DESCRIPTION_CHANGED":
+      return { ...state, description: action.payload };
+    case "TODO_SEARCHED":
+      return { ...state, list: action.payload.data };
+    default:
+      return state;
+  }
+};
